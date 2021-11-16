ddb2dca866c97da57c36a218e24d06275869cb50
romzz.git <romzz.patel@gmail.com>
2021-11-16 04:53:45 -0500

    db.js starter code

---
+++ public/db.js
@@ -0,0 +1,19 @@
+let db;
+// this creates a new db request for the "BudgetDB"
+const request = window.indexedDB.open("BudgetDB", 1);
+
+// this creates an objectstore with auto increment set to true
+request.onupgradeneeded = ({ target }) => {
+    const db = target.result
+    db.createObjectStore("BudgetStore", { autoIncrement: true });
+}
+request.onsuccess = (event) => {
+    db = event.target.result;
+    //checks if the navigator is online
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+}
+request.onerror = (event) => {
+    console.log(event.target.errorCode)
+}
